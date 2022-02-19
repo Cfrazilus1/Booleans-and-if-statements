1ade0ae295c2ca11ae2318a2187cd8de00528821
Cfrazilus1 <99999698+Cfrazilus1@users.noreply.github.com>
2022-02-19 20:32:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+day = 19
+weight = 329.8
+month = "February"
+
+light_is_on = False
+
+if light_is_on:
+  print ("The light is on!")
+  print ("Hello")
+print ("Howdy")
+
+TV_is_on = True
+
+if TV_is_on:
+  print ("Friends is playing")
